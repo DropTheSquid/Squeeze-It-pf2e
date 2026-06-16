2d7cd50faaf6d924db38ec05aedec5cf27201e27
DropTheSquid <93409800+DropTheSquid@users.noreply.github.com>
2026-06-16 17:07:32 -0600

    fixed missing end quote.

--- squeeze-it/scripts/squeeze-manager.js
+++ squeeze-it/scripts/squeeze-manager.js
@@ -3,7 +3,7 @@ import { CreatureSize } from "./constants.js";
 export class SqueezeManager {
 
   static compareActorToTarget(actor, targetSize) {
-    const actorSize = CreatureSize.fromString(game.system.id === "pf2e ? actor?.system?.traits?.size?.value : actor?.system?.traits?.size);
+    const actorSize = CreatureSize.fromString(game.system.id === "pf2e" ? actor?.system?.traits?.size?.value : actor?.system?.traits?.size);
     if (actorSize === null) return "unknown";
 
     if (actorSize < targetSize) return "smaller";
